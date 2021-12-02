fe10afcc08506f647a0bc7773adcb75332cb7ce1
verzelea <arthurverzele@hotmail.com>
2021-12-03 00:46:57 +0100

    feat: start form

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,16 @@
 class StoriesController < ApplicationController
+  before_action :find_story, only: %i[show edit update destroy]
   def index
    @stories = Story.order(:date)
   end
+  def new
+    @Story=Story.new
+  end
+  def create
+  end
+  def find_story
+    @story=Story.find(params[:id])
+  end
+
  end
  
